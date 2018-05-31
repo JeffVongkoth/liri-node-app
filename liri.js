c1975bd41306057b693af8b914a5af84ee8db121
Jeff Vongkoth <jvongkoth@gmail.com>
2018-05-31 14:58:27 -0500

    finished spotify function

--- liri.js
+++ liri.js
@@ -5,11 +5,13 @@ var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
+var omdb = require('omdb');
 var fs = require('fs');
-// console.log(process.env.SPOTIFY_ID);
+
 // npm i --save twitter node-spotify-api request fs ./keys.js
+
 //Api keys      
-var spotifyKeys = new Spotify(keys.spotify);
+// var spotifyKeys = new Spotify(keys.spotifynode);
 var client = new Twitter(keys.twitter);
 
 // console.log(spotifyKeys);
@@ -18,52 +20,67 @@ var client = new Twitter(keys.twitter);
 
 var nodeArgs = process.argv;
 var command = nodeArgs[2];
-var song = '';
+var musicSearch = '';
 var movie = '';
 
-    
-for (var i = 2; i < nodeArgs.length; i++) {
+
+for (var i = 3; i < nodeArgs.length; i++) {
 
     // Build a string with the song.
-    song = song + nodeArgs[i] + " " ;
+    musicSearch = musicSearch + nodeArgs[i] + " ";
 }
-console.log(song);
-function findSong() {
-spotifyKeys.then({ type: 'track', query:song }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-    console.log(data[0]); 
-    console.log(spotifyKeys);
-});
+
+
+function findMusic() {
+    console.log(musicSearch);
+    var spotify = new Spotify({
+        id: '672a0b3753a0418eb02c9caf9bba5844',
+        secret: '11868d2d86404d889b67ce8230be2c4d'
+    });
+
+    spotify.search({ type: 'track', query: musicSearch }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        else if (data.tracks.items.length > 1) {
+            for (let i = 0; i < data.tracks.items.length; i++) {
+                var artists = [];
+                for (var counter = 0; counter < data.tracks.items[i].artists.length; counter++) {
+                    artists.push(data.tracks.items[i].artists[counter].name)
+                }
+                console.log('Artists: ' + artists.join(', ') + "; ")
+                console.log('Song: ' + data.tracks.items[i].name);
+                console.log('Spotify Link: ' + data.tracks.items[i].preview_url);
+                console.log('Album: ' + data.tracks.items[i].album.name);
+                console.log('');
+            }
+        }
+        else {
+            musicSearch = 'rick astley never gonna give you up';
+            findMusic();
+        }
+    });
 
 };
-// console.log(spotifyKeys);
-// console.log(spotifyKeys.credentials.id);
-// console.log(spotifyKeys.credentials.secret);
-
 
 
 
 function tweetFeed() {
-    var params = {screen_name: 'gerphmoonboy'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
+    var params = { screen_name: 'gerphmoonboy' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
 
-      }
+        }
         for (var i = 0; i < tweets.length; i++) {
             console.log(tweets[i].text);
         }
     });
 };
 
-if (command === 'my-tweets'){
+if (command === 'my-tweets') {
     tweetFeed();
 };
 
-if(command === 'spotify-this'){
-    findSong();
+if (command === 'spotify-this') {
+    findMusic();
 };
-
-
